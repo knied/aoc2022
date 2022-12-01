2afd2d02385f718c4708e493716bca0d1394af5b
Kristof Niederholtmeyer <kristof@Kristofs-MacBook-Air.fritz.box>
2022-12-01 06:45:23 +0100

    Solution for day 1a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("input/day1a.txt")
+        .expect("Unable to read input file.");
+    let mut max_calories : i32 = 0;
+    let mut elf_calories : i32 = 0;
+    for line in content.split("\n") {
+        if let Some(calories) = line.parse::<i32>().ok() {
+            elf_calories += calories;
+        } else {
+            println!("elf calories: {}", elf_calories);
+            if elf_calories > max_calories {
+                max_calories = elf_calories;
+            }
+            elf_calories = 0;
+        }
+    }
+    println!("max calories: {}", max_calories);
 }
