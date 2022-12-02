a0a252f153a8cf5aa9f2495c983eea623499d053
Kristof Niederholtmeyer <kristof@Kristofs-MacBook-Air.fritz.box>
2022-12-02 20:38:35 +0100

    Solution for day 2b

--- src/main.rs
+++ src/main.rs
@@ -31,29 +31,58 @@ fn score(elf : &Option<HandSign>, me : &Option<HandSign>) -> Option<i32> {
     }
 }
 
+enum Goal {
+    Lose,
+    Draw,
+    Win,
+}
+
+fn response(goal : Goal, elf : &Option<HandSign>) -> Option<HandSign> {
+    match elf {
+        Some(HandSign::Rock) => match goal {
+            Goal::Lose => Some(HandSign::Scissor),
+            Goal::Draw => Some(HandSign::Rock),
+            Goal::Win => Some(HandSign::Paper),
+        },
+        Some(HandSign::Paper) => match goal {
+            Goal::Lose => Some(HandSign::Rock),
+            Goal::Draw => Some(HandSign::Paper),
+            Goal::Win => Some(HandSign::Scissor),
+        },
+        Some(HandSign::Scissor) => match goal {
+            Goal::Lose => Some(HandSign::Paper),
+            Goal::Draw => Some(HandSign::Scissor),
+            Goal::Win => Some(HandSign::Rock),
+        },
+        None => None,
+    }
+}
+
+fn round(line : &str) -> (Option<HandSign>, Option<HandSign>){
+    let chars : Vec<&str> = line.split_whitespace().collect();
+    let elf = match chars[0] {
+        "A" => Some(HandSign::Rock), // rock
+        "B" => Some(HandSign::Paper), // paper
+        "C" => Some(HandSign::Scissor), // scissor
+        _ => None,
+    };
+    let me = match chars[1] {
+        "X" => response(Goal::Lose, &elf), // need to lose
+        "Y" => response(Goal::Draw, &elf), // need to draw
+        "Z" => response(Goal::Win, &elf), // need to win
+        _ => None,
+    };
+    return (elf, me);
+}
+
 fn main() {
     let content = fs::read_to_string("input/day2a.txt")
         .expect("Unable to read input file.");
 
     let res : Option<i32> = content
         .lines()
-        .map(|line| -> Option<i32> {
-            let round : Vec<Option<HandSign>>= line.split_whitespace()
-                .map(|sign| -> Option<HandSign> {
-                    match sign {
-                        "A" => Some(HandSign::Rock), // rock
-                        "B" => Some(HandSign::Paper), // paper
-                        "C" => Some(HandSign::Scissor), // scissor
-
-                        "X" => Some(HandSign::Rock), // rock
-                        "Y" => Some(HandSign::Paper), // paper
-                        "Z" => Some(HandSign::Scissor), // scissor
-
-                        _ => None,
-                    }
-                }).collect();
-            return score(&round[0], &round[1]);
-        })
+        .map(|line| round(line))
+        .map(|(elf, me)| score(&elf, &me))
         .sum();
     println!("{:?}", res);
 }
