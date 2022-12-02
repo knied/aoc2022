f9625c63b916bd151dc3b064a212b5ee46ca6508
Kristof Niederholtmeyer <kristof@Kristofs-MacBook-Air.fritz.box>
2022-12-02 22:01:31 +0100

    Even more compact ;)

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,10 @@ fn main() {
     let res : usize = content
         .lines()
         .map(|line| {
-            let c : Vec<usize> = line.split_whitespace()
-                .map(|s| s.chars().map(|c| c as usize).sum::<usize>())
-                .collect();
+            let c : Vec<usize> = line.chars().map(|c| c as usize).collect();
             let elf = c[0] - ('A' as usize);
-            //let me = c[1] - ('X' as usize);
-            let me = ((c[1] - ('X' as usize) + 2) % 3 + elf) % 3;
+            //let me = c[2] - ('X' as usize);
+            let me = ((c[2] - ('X' as usize) + 2) % 3 + elf) % 3;
             return (((4 - elf) % 3 + me) % 3) * 3 + me + 1;
         }).sum();
 
