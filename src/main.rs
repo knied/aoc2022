8b12803bfdc3fcff8a3990aff10f582ffdf1bd36
Kristof Niederholtmeyer <kristof@Kristofs-MacBook-Air.fritz.box>
2022-12-03 07:37:25 +0100

    Solution for day 3a

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 use std::fs;
 
+fn prio(item : u8) -> u32 {
+    if (item - 'A' as u8).lt(&26) {
+        return (item - 'A' as u8) as u32 + 27;
+    } else {
+        return (item - 'a' as u8) as u32 + 1;
+    }
+}
+
 fn main() {
-    let content = fs::read_to_string("input/day2a.txt")
+    let content = fs::read_to_string("input/day3a.txt")
         .expect("Unable to read input file.");
 
-    let res : usize = content
+    let res : u32 = content
         .lines()
         .map(|line| {
-            let c : Vec<usize> = line.chars().map(|c| c as usize).collect();
-            let elf = c[0] - ('A' as usize);
-            //let me = c[2] - ('X' as usize);
-            let me = ((c[2] - ('X' as usize) + 2) % 3 + elf) % 3;
-            return (((4 - elf) % 3 + me) % 3) * 3 + me + 1;
-        }).sum();
-
+            let items : Vec<u8> = line.chars().map(|c| c as u8).collect();
+            let item_count = items.len();
+            let compartment_size = item_count / 2;
+            for i in 0..compartment_size {
+                let item = items[i];
+                for j in compartment_size..item_count {
+                    if items[j] == item {
+                        return prio(item);
+                    }
+                }
+            }
+            panic!("no duplicate item");
+        })
+        .sum();
     println!("{}", res);
 }
