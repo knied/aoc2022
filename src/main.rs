5138dfb1d71710d3f8d996e8beb570635bb49257
Kristof Niederholtmeyer <kristof@Kristofs-MacBook-Air.fritz.box>
2022-12-03 08:46:50 +0100

    Solution for day 3b

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,24 @@ fn main() {
     let content = fs::read_to_string("input/day3a.txt")
         .expect("Unable to read input file.");
 
-    let res : u32 = content
-        .lines()
-        .map(|line| {
-            let items : Vec<u8> = line.chars().map(|c| c as u8).collect();
-            let item_count = items.len();
-            let compartment_size = item_count / 2;
-            for i in 0..compartment_size {
-                let item = items[i];
-                for j in compartment_size..item_count {
-                    if items[j] == item {
-                        return prio(item);
-                    }
+    let mut sum : u32 = 0;
+    let mut lines = content.lines();
+    loop {
+        let group = lines.by_ref().take(3)
+            .map(|line| -> Vec<u32> {
+                return line.chars().map(|c| prio(c as u8)).collect()
+            });
+        let rucksacks : Vec<Vec<u32>> = group.collect();
+        if rucksacks.is_empty() {
+            break;
+        }
+        for item in &rucksacks[0] {
+            if rucksacks.iter().skip(1).map(|r| r.contains(item))
+                .fold(true, |is_badge,in_rucksack| is_badge && in_rucksack) {
+                    sum += item;
+                    break;
                 }
-            }
-            panic!("no duplicate item");
-        })
-        .sum();
-    println!("{}", res);
+        }
+    }
+    println!("{}", sum)
 }
