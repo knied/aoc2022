ed2667b5f5e1ebc07f177cc2c4a2fa0d29557d7e
Kristof Niederholtmeyer <kristof@Kristofs-MacBook-Air.fritz.box>
2022-12-03 18:26:19 +0100

    clippy and rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::fs;
 
-fn prio(item : u8) -> u32 {
-    if (item - 'A' as u8).lt(&26) {
-        return (item - 'A' as u8) as u32 + 27;
+fn prio(item: &u8) -> u32 {
+    if (item - b'A').lt(&26) {
+        (item - b'A') as u32 + 27
     } else {
-        return (item - 'a' as u8) as u32 + 1;
+        (item - b'a') as u32 + 1
     }
 }
 
@@ -12,23 +12,28 @@ fn main() {
     let content = fs::read_to_string("input/day3a.txt")
         .expect("Unable to read input file.");
 
-    let mut sum : u32 = 0;
+    let mut sum: u32 = 0;
     let mut lines = content.lines();
+
     loop {
-        let group = lines.by_ref().take(3)
-            .map(|line| -> Vec<u32> {
-                return line.chars().map(|c| prio(c as u8)).collect()
-            });
-        let rucksacks : Vec<Vec<u32>> = group.collect();
+        let group = lines
+            .by_ref()
+            .take(3)
+            .map(|line| line.chars().map(|c| c as u8).collect());
+        let rucksacks: Vec<Vec<u8>> = group.collect();
         if rucksacks.is_empty() {
             break;
         }
         for item in &rucksacks[0] {
-            if rucksacks.iter().skip(1).map(|r| r.contains(item))
-                .fold(true, |is_badge,in_rucksack| is_badge && in_rucksack) {
-                    sum += item;
-                    break;
-                }
+            if rucksacks
+                .iter()
+                .skip(1)
+                .map(|r| r.contains(item))
+                .all(|in_rucksack| in_rucksack)
+            {
+                sum += prio(item);
+                break;
+            }
         }
     }
     println!("{}", sum)
