d2c6d2fb96920167da9d1c8837ba4e3f4e1d4b19
Kristof Niederholtmeyer <kristof@Kristofs-MacBook-Air.fritz.box>
2022-12-04 20:48:20 +0100

    Solution for day 4a

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,49 @@
 use std::fs;
+use std::ops::Range;
 
-fn prio(item: &u8) -> u32 {
-    if (item - b'A').lt(&26) {
-        (item - b'A') as u32 + 27
-    } else {
-        (item - b'a') as u32 + 1
+fn merge<T: std::cmp::Ord>(r0: Range<T>, r1: Range<T>) -> Range<T> {
+    Range {
+        start: std::cmp::min::<T>(r0.start, r1.start),
+        end: std::cmp::max::<T>(r0.end, r1.end),
     }
 }
 
+fn contained(r0: &Range<u32>, r1: &Range<u32>) -> bool {
+    r0.contains(&r1.start) && r0.contains(&(r1.end - 1))
+}
+
 fn main() {
-    let content = fs::read_to_string("input/day3a.txt")
+    let content = fs::read_to_string("input/day4a.txt")
         .expect("Unable to read input file.");
 
-    let mut sum: u32 = 0;
-    let mut lines = content.lines();
-
-    loop {
-        let group = lines
-            .by_ref()
-            .take(3)
-            .map(|line| line.chars().map(|c| c as u8).collect());
-        let rucksacks: Vec<Vec<u8>> = group.collect();
-        if rucksacks.is_empty() {
-            break;
-        }
-        for item in &rucksacks[0] {
-            if rucksacks
-                .iter()
-                .skip(1)
-                .map(|r| r.contains(item))
-                .all(|in_rucksack| in_rucksack)
-            {
-                sum += prio(item);
-                break;
-            }
-        }
-    }
-    println!("{}", sum)
+    let count = content
+        .lines()
+        .map(|line| -> bool {
+            let ranges = line
+                .split(',')
+                .map(|elf| {
+                    elf.split('-')
+                        .map(|s| s.parse::<u32>().expect("must be number"))
+                        .map(|n| Range::<u32> {
+                            start: n,
+                            end: n + 1,
+                        })
+                        .reduce(merge)
+                        .expect("must be valid ranges")
+                })
+                .collect::<Vec<_>>();
+            contained(&ranges[0], &ranges[1])
+                || contained(&ranges[1], &ranges[0])
+        })
+        .fold(
+            0_u32,
+            |count, fully_contained| {
+                if fully_contained {
+                    count + 1
+                } else {
+                    count
+                }
+            },
+        );
+    println!("{}", count);
 }
