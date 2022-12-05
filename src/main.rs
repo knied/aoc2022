9d9b33f66c8b39b6eebc5ce8240d2d299bea2085
Kristof Niederholtmeyer <950425+knied@users.noreply.github.com>
2022-12-05 19:16:37 +0100

    Solution for day 5a

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,96 @@
+use std::fmt;
 use std::fs;
-use std::ops::Range;
 
-fn merge<T: std::cmp::Ord>(r0: Range<T>, r1: Range<T>) -> Range<T> {
-    Range {
-        start: std::cmp::min::<T>(r0.start, r1.start),
-        end: std::cmp::max::<T>(r0.end, r1.end),
+struct Storage {
+    stacks: Vec<Vec<char>>,
+}
+
+trait Stacks<T> {
+    fn put(&mut self, idx: usize, item: T);
+    fn take(&mut self, idx: usize) -> Option<T>;
+}
+
+impl Stacks<char> for Storage {
+    fn put(&mut self, idx: usize, item: char) {
+        if idx >= self.stacks.len() {
+            self.stacks.resize(idx + 1, Vec::<char>::new())
+        }
+        self.stacks[idx].push(item);
+    }
+    fn take(&mut self, idx: usize) -> Option<char> {
+        if idx >= self.stacks.len() {
+            None
+        } else {
+            self.stacks[idx].pop()
+        }
     }
 }
 
-fn overlap(r0: &Range<u32>, r1: &Range<u32>) -> bool {
-    (r0.start as i32 - r1.end as i32) < 0
-        && (r0.end as i32 - r1.start as i32) > 0
+impl std::str::FromStr for Storage {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut ret = Storage {
+            stacks: Vec::<Vec<char>>::new(),
+        };
+        s.lines()
+            .rev()
+            .skip(1)
+            .map(|line| {
+                let mut idx: usize = 0;
+                line.chars()
+                    .collect::<Vec<_>>()
+                    .chunks(4)
+                    .map(|c| c[1])
+                    .all(|item| {
+                        if item != ' ' {
+                            ret.put(idx, item);
+                        }
+                        idx += 1;
+                        true
+                    })
+            })
+            .all(|_| true);
+        Ok(ret)
+    }
+}
+
+impl std::fmt::Display for Storage {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut res = String::new();
+        for s in &self.stacks {
+            res.push(*s.last().expect("there should be at least one box"));
+        }
+        write!(f, "{}", res)
+    }
 }
 
 fn main() {
-    let content = fs::read_to_string("input/day4a.txt")
+    let content = fs::read_to_string("input/day5a.txt")
         .expect("Unable to read input file.");
 
-    let count = content
+    let parts = content.split("\n\n").collect::<Vec<_>>();
+
+    let mut storage = parts[0].parse::<Storage>().expect("should be storage");
+    parts[1]
         .lines()
-        .map(|line| -> bool {
-            let ranges = line
-                .split(',')
-                .map(|elf| {
-                    elf.split('-')
-                        .map(|s| s.parse::<u32>().expect("must be number"))
-                        .map(|n| Range::<u32> {
-                            start: n,
-                            end: n + 1,
-                        })
-                        .reduce(merge)
-                        .expect("must be valid ranges")
-                })
-                .collect::<Vec<_>>();
-            overlap(&ranges[0], &ranges[1])
+        .map(|line| {
+            line.split_whitespace()
+                .collect::<Vec<_>>()
+                .chunks(2)
+                .map(|c| c[1].parse::<usize>().expect("should be a number"))
+                .collect::<Vec<_>>()
         })
-        .filter(|b| *b)
-        .count();
-    println!("{}", count);
+        .all(|instr| {
+            let from = instr[1] - 1;
+            let to = instr[2] - 1;
+            for _ in 0..instr[0] {
+                let item =
+                    storage.take(from).expect("stack should not be empty");
+                storage.put(to, item);
+            }
+            true
+        });
+
+    println!("{}", storage);
 }
