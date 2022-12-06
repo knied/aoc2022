36dd78b71ccbcea0f979d0d3162e6ce370a5660a
Kristof Niederholtmeyer <950425+knied@users.noreply.github.com>
2022-12-06 06:51:58 +0100

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::fs;
 use std::collections::HashSet;
+use std::fs;
 
-const MARKER_LEN : usize = 14;
+const MARKER_LEN: usize = 14;
 fn main() {
     let content = fs::read_to_string("input/day6a.txt")
         .expect("Unable to read input file.");
 
     let msg = content.chars().collect::<Vec<_>>();
     for idx in 0..msg.len() {
-        let unique = msg.iter().skip(idx).take(MARKER_LEN).collect::<HashSet<_>>().len();
+        let unique = msg
+            .iter()
+            .skip(idx)
+            .take(MARKER_LEN)
+            .collect::<HashSet<_>>()
+            .len();
         if unique == MARKER_LEN {
-            println!("{}", idx+MARKER_LEN);
+            println!("{}", idx + MARKER_LEN);
         }
     }
 }
