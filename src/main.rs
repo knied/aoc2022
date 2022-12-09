0d3f76340ad82a6107c982381352d23b01ce18e5
Kristof Niederholtmeyer <950425+knied@users.noreply.github.com>
2022-12-09 23:00:16 +0100

    Solution for day 9a/b

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,71 @@
 use std::fs;
+use std::ops::*;
+use std::collections::HashSet;
 
-fn score1(i: usize, heights: &Vec<i32>) -> (bool, usize) {
-    let height = heights[i];
-    let left_oversee = heights
-        .iter()
-        .take(i)
-        .rev()
-        .take_while(|h| **h < height)
-        .count();
-    let left = if left_oversee < i {
-        (false, left_oversee + 1) // +1 for the tree that is blocking our view
-    } else {
-        (true, left_oversee)
-    };
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+struct V2(i32, i32);
 
-    let right_oversee = heights
-        .iter()
-        .skip(i + 1)
-        .take_while(|h| **h < height)
-        .count();
-    let right = if right_oversee < (heights.len() - i - 1) {
-        (false, right_oversee + 1) // +1 for the tree that is blocking our view
-    } else {
-        (true, right_oversee)
-    };
-    (left.0 || right.0, left.1 * right.1)
+impl Add for V2 {
+    type Output = V2;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        V2(self.0 + rhs.0, self.1 + rhs.1)
+    }
 }
 
-type Heights = Vec<Vec<i32>>;
-fn score2(r: usize, c: usize, heights: &Heights) -> (bool, usize) {
-    let row = &heights[r];
-    let h = score1(c, row);
-    let col = heights.iter().map(|row| row[c]).collect::<Vec<_>>();
-    let v = score1(r, &col);
-    (h.0 || v.0, h.1 * v.1)
+impl Sub for V2 {
+    type Output = V2;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        V2(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl Div for V2 {
+    type Output = V2;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        V2(self.0 / rhs.0, self.1 / rhs.1)
+    }
 }
 
 fn main() {
-    let content = fs::read_to_string("input/day8a.txt")
+    let content = fs::read_to_string("input/day9a.txt")
         .expect("Unable to read input file.");
 
-    let heights = content
+    let movements = content
         .lines()
         .map(|line| {
-            line.chars()
-                .map(|c| c as i32 - b'0' as i32)
-                .collect::<Vec<_>>()
+            let t = line
+                .split_whitespace()
+                .collect::<Vec<_>>();
+            let count = t[1]
+                .parse::<u32>()
+                .expect("invalid input");
+            let direction = match t[0] {
+                "L" => V2(-1,0),
+                "R" => V2(1,0),
+                "U" => V2(0,1),
+                "D" => V2(0,-1),
+                _ => panic!("invalid input")
+            };
+            (direction, count)
         })
         .collect::<Vec<_>>();
 
-    let mut count: usize = 0;
-    let mut best_score: usize = 0;
-    for r in 0..heights.len() {
-        for c in 0..heights[r].len() {
-            let (visible, score) = score2(r, c, &heights);
-            best_score = std::cmp::max(best_score, score);
-            if visible {
-                count += 1;
-                print!("{}", heights[r][c]);
-            } else {
-                print!(" ");
+    let mut visited = HashSet::<V2>::new();
+    let mut rope : [V2; 10] = [V2(0,0); 10];
+    for m in movements {
+        for _ in 0..m.1 {
+            rope[0] = rope[0] + m.0;
+            for i in 0..9 {
+                let d = (rope[i] - rope[i+1]) / V2(2,2);
+                if d != V2(0,0) {
+                    rope[i+1] = rope[i] - d;
+                }
             }
+            visited.insert(rope[9]);
         }
-        println!();
     }
-    println!("{} of {}", count, heights.len() * heights[0].len());
-    println!("best score: {}", best_score);
+    println!("{}", visited.len());
 }
