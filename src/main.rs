268c77df48f1ad77b139db7c1c31eeecd338b5b1
Kristof Niederholtmeyer <950425+knied@users.noreply.github.com>
2022-12-10 09:12:52 +0100

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -16,36 +16,36 @@ fn main() {
             match tokens[0] {
                 "noop" => Instr::Noop,
                 "addx" => Instr::Addx(
-                    tokens[1].parse::<i32>().expect("should be an i32")),
-                _ => panic!("invalid instruction")
+                    tokens[1].parse::<i32>().expect("should be an i32"),
+                ),
+                _ => panic!("invalid instruction"),
             }
         })
         .collect::<Vec<_>>();
 
-    let mut x : i32 = 1;
-    let mut cycle : i32 = 0;
-    instr
-        .iter().for_each(|i| {
-            let mut next_x = x;
-            let cycles = match i {
-                Instr::Noop => 1,
-                Instr::Addx(v) => {
-                    next_x += v;
-                    2
-                }
-            };
-            for _ in 0..cycles {
-                if x - 1 <= cycle && x + 1 >= cycle {
-                    print!("#");
-                } else {
-                    print!(".");
-                }
-                cycle += 1;
-                if cycle >= 40 {
-                    println!();
-                    cycle = 0;
-                }
+    let mut x: i32 = 1;
+    let mut cycle: i32 = 0;
+    instr.iter().for_each(|i| {
+        let mut next_x = x;
+        let cycles = match i {
+            Instr::Noop => 1,
+            Instr::Addx(v) => {
+                next_x += v;
+                2
             }
-            x = next_x;
-        });
+        };
+        for _ in 0..cycles {
+            if x - 1 <= cycle && x + 1 >= cycle {
+                print!("#");
+            } else {
+                print!(".");
+            }
+            cycle += 1;
+            if cycle >= 40 {
+                println!();
+                cycle = 0;
+            }
+        }
+        x = next_x;
+    });
 }
