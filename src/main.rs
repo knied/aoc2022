9d4665a2b9bbe9286a9562c72d1054205f10fd67
Kristof Niederholtmeyer <950425+knied@users.noreply.github.com>
2022-12-10 08:49:13 +0100

    Solution for day 10a

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,52 @@
 use std::fs;
-use std::ops::*;
-use std::collections::HashSet;
 
-#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
-struct V2(i32, i32);
-
-impl Add for V2 {
-    type Output = V2;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        V2(self.0 + rhs.0, self.1 + rhs.1)
-    }
-}
-
-impl Sub for V2 {
-    type Output = V2;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        V2(self.0 - rhs.0, self.1 - rhs.1)
-    }
-}
-
-impl Div for V2 {
-    type Output = V2;
-
-    fn div(self, rhs: Self) -> Self::Output {
-        V2(self.0 / rhs.0, self.1 / rhs.1)
-    }
+enum Instr {
+    Noop,
+    Addx(i32),
 }
 
 fn main() {
-    let content = fs::read_to_string("input/day9a.txt")
+    let content = fs::read_to_string("input/day10a.txt")
         .expect("Unable to read input file.");
 
-    let movements = content
+    let instr = content
         .lines()
         .map(|line| {
-            let t = line
-                .split_whitespace()
-                .collect::<Vec<_>>();
-            let count = t[1]
-                .parse::<u32>()
-                .expect("invalid input");
-            let direction = match t[0] {
-                "L" => V2(-1,0),
-                "R" => V2(1,0),
-                "U" => V2(0,1),
-                "D" => V2(0,-1),
-                _ => panic!("invalid input")
-            };
-            (direction, count)
+            let tokens = line.split_whitespace().collect::<Vec<_>>();
+            match tokens[0] {
+                "noop" => Instr::Noop,
+                "addx" => Instr::Addx(
+                    tokens[1].parse::<i32>().expect("should be an i32")),
+                _ => panic!("invalid instruction")
+            }
         })
         .collect::<Vec<_>>();
 
-    let mut visited = HashSet::<V2>::new();
-    let mut rope : [V2; 10] = [V2(0,0); 10];
-    for m in movements {
-        for _ in 0..m.1 {
-            rope[0] = rope[0] + m.0;
-            for i in 0..9 {
-                let d = (rope[i] - rope[i+1]) / V2(2,2);
-                if d != V2(0,0) {
-                    rope[i+1] = rope[i] - d;
+    let mut x : i32 = 1;
+    let mut cycle : i32 = 0;
+    let mut next_sample : i32 = 20;
+    let mut result : i32 = 0;
+    instr
+        .iter().for_each(|i| {
+            let mut next_x = x;
+            match i {
+                Instr::Noop => {
+                    cycle += 1;
+                    println!("{}: noop (x = {}))", cycle, x);
+                },
+                Instr::Addx(v) => {
+                    cycle += 2;
+                    next_x += v;
+                    println!("{}: addx {} (x = {} -> {}))", cycle, v, x, next_x);
                 }
+            };
+            
+            while cycle >= next_sample {
+                result += next_sample * x;
+                println!("{}: sample {} (x = {}))", cycle, next_sample, x);
+                next_sample += 40;
             }
-            visited.insert(rope[9]);
-        }
-    }
-    println!("{}", visited.len());
+            x = next_x;
+        });
+    println!("result = {}", result);
 }
